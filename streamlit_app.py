fca32fbddee75813577021aa35c2367802a5bf92
Hongrae  Roh <hongrae@bu.edu>
2024-11-19 11:59:45 -0500

    Icon add

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.set_page_config(page_title="Snap Review", page_icon="img/SnapReviewIcon.png",)
+st.image("img/SnapReviewIcon.png", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto", *, use_container_width=False)
 st.write('Quick Google Review Summary')
 
 # Design move app further up and remove top padding
