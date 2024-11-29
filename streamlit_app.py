b570faa8c9c24ba25dec18e7ce31d30350305483
Hongrae  Roh <156094157+melancholy32@users.noreply.github.com>
2024-11-29 18:53:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from openai import OpenAI
 
 st.set_page_config(page_title="Snap Review", page_icon="img/SnapReviewIcon.png",)
 st.image("img/SnapReviewIcon.png", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=False)
