413737e00d8b1567001fee06617ba123bd5943d9
Hongrae  Roh <156094157+melancholy32@users.noreply.github.com>
2024-11-29 19:08:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-python -m pip install --upgrade streamlit-extras
+py -m pip install -r requirements.txt
 
 st.set_page_config(page_title="Snap Review", page_icon="img/SnapReviewIcon.png",)
 st.image("img/SnapReviewIcon.png", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=False)
