06c7439e0d7e2d7c14fd8098531cef33847e9fca
Hongrae  Roh <156094157+melancholy32@users.noreply.github.com>
2024-11-29 19:10:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,31 +5,6 @@ st.set_page_config(page_title="Snap Review", page_icon="img/SnapReviewIcon.png",
 st.image("img/SnapReviewIcon.png", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=False)
 st.title("Quick Google Review Summary")
 
-# Design move app further up and remove top padding
-st.markdown('''<style>.css-1egvi7u {margin-top: -4rem;}</style>''',
-    unsafe_allow_html=True)
-# Design change hyperlink href link color
-st.markdown('''<style>.css-znku1x a {color: #CDF6F9;}</style>''',
-    unsafe_allow_html=True)  # darkmode
-st.markdown('''<style>.css-znku1x a {color: #CDF6F9;}</style>''',
-    unsafe_allow_html=True)  # lightmode
-# Design change height of text input fields headers
-st.markdown('''<style>.css-qrbaxs {min-height: 0.0rem;}</style>''',
-    unsafe_allow_html=True)
-# Design change spinner color to primary color
-st.markdown('''<style>.stSpinner > div > div {border-top-color: #9d03fc;}</style>''',
-    unsafe_allow_html=True)
-# Design change min height of text input box
-st.markdown('''<style>.css-15tx938{min-height: 0.0rem;}</style>''',
-    unsafe_allow_html=True)
-# Design hide top header line
-hide_decoration_bar_style = '''<style>header {visibility: hidden;}</style>'''
-st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
-# Design hide "made with streamlit" footer menu area
-hide_streamlit_footer = """<style>#MainMenu {visibility: hidden;}
-                        footer {visibility: hidden;}</style>"""
-st.markdown(hide_streamlit_footer, unsafe_allow_html=True)
-
-from streamlit_extras.switch_page_button import switch_page
+import switch_page
 
 switch_page("pages/page2.py")
