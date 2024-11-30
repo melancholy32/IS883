272b368a784118bbad382b7eff6864fcbeed9a50
Hongrae  Roh <156094157+melancholy32@users.noreply.github.com>
2024-11-29 19:29:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,5 @@
 import streamlit as st
-import openai
 
 st.set_page_config(page_title="Snap Review", page_icon="img/SnapReviewIcon.png")
 st.image("img/SnapReviewIcon.png", caption=None)
 st.title("Quick Google Review Summary")
-
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    st.markdown("[Get an OpenAI API key](https://platform.openai.com/account/api-keys)")
-    st.markdown("[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)")
-    st.markdown("[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)")
-
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "system", "content": "You are a helpful assistant."}]
-
-for msg in st.session_state["messages"]:
-    st.chat_message(msg["role"]).write(msg["content"])
-
-if prompt := st.chat_input():
-    if not openai_api_key:
-        st.warning("Please provide a valid OpenAI API key.")
-        st.stop()
-
-    openai.api_key = openai_api_key
-    st.session_state["messages"].append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-
-    try:
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=st.session_state["messages"]
-        )
-        msg = response.choices[0].message["content"]
-        st.session_state["messages"].append({"role": "assistant", "content": msg})
-        st.chat_message("assistant").write(msg)
-    except Exception as e:
-        st.error(f"Error: {str(e)}")
