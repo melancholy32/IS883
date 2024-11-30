8cdcb4c3b2621ce55fa5884e235f800c9bf71116
Hongrae  Roh <156094157+melancholy32@users.noreply.github.com>
2024-11-29 19:11:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,3 @@ import streamlit as st
 st.set_page_config(page_title="Snap Review", page_icon="img/SnapReviewIcon.png",)
 st.image("img/SnapReviewIcon.png", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=False)
 st.title("Quick Google Review Summary")
-
-import switch_page
-
-switch_page("pages/page2.py")
