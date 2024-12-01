76b376de184e2d4928cb0e595a7f70a9e986d2a2
Hongrae  Roh <156094157+melancholy32@users.noreply.github.com>
2024-12-01 01:31:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -163,7 +163,7 @@ else:
 
 store_type = ["Restaurant", "Bar", "Cafe"]
 selected_index = button_selector(store_type, index=0, spec=4, key="button_selector_place_type", label="What kind of place are you looking for?")
-st.write(f"Selected month: {place_type[selected_index]}")
+st.write(f"Selected month: {store_type[selected_index]}")
 
 #store_type = st.selectbox(
 #    "What type of the place you are looking for?",
