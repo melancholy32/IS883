b6c2e36183f7b615e81806d788c026fdf628ba2a
PaigeYang <33799289+PaigeYang@users.noreply.github.com>
2024-12-06 13:10:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,6 +63,7 @@ def fetch_reviews_summary(reviews):
             stop=None,
             temperature=0.7,
         )
+        st.write(response)
         summary_str = response['choices'][0]['message']['content']
 
         match = re.search(r'\{(.*?)\}', summary_str, re.DOTALL)
